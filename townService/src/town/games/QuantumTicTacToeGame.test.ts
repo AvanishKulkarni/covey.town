b90492ad8f01f2a96e82459a6a7d941cae79cdc9
Avanish Kulkarni <55012494+AvanishKulkarni@users.noreply.github.com>
2025-09-19 12:22:33 -0400

    test join

--- townService/src/town/games/QuantumTicTacToeGame.test.ts
+++ townService/src/town/games/QuantumTicTacToeGame.test.ts
@@ -1,17 +1,25 @@
+import { throws } from 'node:assert';
+import exp from 'node:constants';
 import { createPlayerForTesting } from '../../TestUtils';
 import Player from '../../lib/Player';
 import { GameMove } from '../../types/CoveyTownSocket';
 import QuantumTicTacToeGame from './QuantumTicTacToeGame';
+import {
+  GAME_FULL_MESSAGE,
+  PLAYER_ALREADY_IN_GAME_MESSAGE,
+} from '../../lib/InvalidParametersError';
 
 describe('QuantumTicTacToeGame', () => {
   let game: QuantumTicTacToeGame;
   let player1: Player;
   let player2: Player;
+  let dummyPlayer: Player;
 
   beforeEach(() => {
     game = new QuantumTicTacToeGame();
     player1 = createPlayerForTesting();
     player2 = createPlayerForTesting();
+    dummyPlayer = createPlayerForTesting();
   });
 
   describe('_join', () => {
@@ -20,6 +28,53 @@ describe('QuantumTicTacToeGame', () => {
       expect(game.state.x).toBe(player1.id);
       expect(game.state.o).toBeUndefined();
       expect(game.state.status).toBe('WAITING_TO_START');
+    });
+    it('should add the second player as O', () => {
+      game.join(player1);
+      game.join(player2);
+      expect(game.state.x).toBe(player1.id);
+      expect(game.state.o).toBe(player2.id);
+      expect(game.state.status).toBe('IN_PROGRESS');
+    });
+    it('should throw error if game is full', () => {
+      game.join(player1);
+      game.join(player2);
+      expect(() => game.join(dummyPlayer)).toThrowError(GAME_FULL_MESSAGE);
+    });
+    it('should throw error is player 1 already in game', () => {
+      game.join(player1);
+      expect(() => game.join(player1)).toThrowError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+    });
+    it('should throw error is player 2 already in game', () => {
+      game.join(player2);
+      expect(() => game.join(player2)).toThrowError(PLAYER_ALREADY_IN_GAME_MESSAGE);
+    });
+
+    describe('When the player can be added', () => {
+      it('makes the first player X and inits the state', () => {
+        const player = createPlayerForTesting();
+        game.join(player);
+        expect(game.state.x).toBe(player.id);
+        expect(game.state.o).toBeUndefined();
+        expect(game.state.moves).toHaveLength(0);
+        expect(game.state.status).toBe('WAITING_TO_START');
+        expect(game.state.winner).toBeUndefined();
+      });
+      describe('When the second player joins', () => {
+        beforeEach(() => {
+          game.join(player1);
+          game.join(player2);
+        });
+        it('makes the second player O', () => {
+          expect(game.state.x).toBe(player1.id);
+          expect(game.state.o).toBe(player2.id);
+        });
+        it('sets the game status to IN_PROGRESS', () => {
+          expect(game.state.status).toBe('IN_PROGRESS');
+          expect(game.state.winner).toBeUndefined();
+          expect(game.state.moves).toHaveLength(0);
+        });
+      });
     });
   });
 
